1469fe8cdce2a6cb9ab576ef3a94d579c911fcf0
regretofice <1651979037@qq.com>
2025-12-23 22:40:29 +0800

    将building的构造函数与buildingManager相关联

--- Clash_of_Clans/Classes/Building.cpp
+++ Clash_of_Clans/Classes/Building.cpp
@@ -68,34 +68,31 @@ bool Building::init(const std::string& texPath, const std::string& name,
 
   // 注册每帧更新
   this->scheduleUpdate();
-
+  BuildingManager::addBuilding(this);
   return true;
 }
 
 void Building::checkDestroyed() {
   if (_currentHP <= 0 && !_isDestroyed) {
     _isDestroyed = true;
-    
+
     // 创建废墟图片
     _ruinsSprite = Sprite::create("ruins.png");
     if (_ruinsSprite) {
       // 设置废墟图片的位置和大小与原建筑相同
       _ruinsSprite->setPosition(this->getPosition());
       _ruinsSprite->setAnchorPoint(Vec2(0.5f, 0.5f));
-      
-      
-      
+
       // 将废墟图片添加到父节点，设置较低的Z顺序，确保在士兵下方
       if (this->getParent()) {
         // 士兵的Z顺序通常是5，所以废墟设置为3，确保在士兵下方但在地图上方
         this->getParent()->addChild(_ruinsSprite, 3);
       }
     }
-    
+
     // 隐藏原建筑模型
     this->setVisible(false);
     CCLOG("建筑[%s]被摧毁！", _name.c_str());
-    // 可扩展：播放摧毁动画、通知游戏管理器等
   }
 }
 
@@ -132,6 +129,3 @@ bool Building::upgrade() {
 void Building::update(float dt) {
   Sprite::update(dt);  // 基类更新（空实现，子类重写）
 }
-
-
-
